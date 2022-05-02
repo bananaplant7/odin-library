457d39ed44bc6e721ba918413e322d00f7b2766e
bananaplant7 <brooklyncheng2002@gmail.com>
2022-05-01 23:46:01 -0700

    add ability to add books from pop-up modal

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const modal = document.querySelector('#modal');
 const closeBtn = document.querySelector('#closeBtn');
 const submitBtn = document.querySelector('#submitBtn');
 const overlay = document.querySelector('#overlay');
+const title = document.querySelector('#bookTitle');
+const author = document.querySelector('#authorName');
+const pages = document.querySelector('input[type=number]');
+const read = document.querySelector('#read');
+const notRead = document.querySelector('#notRead');
+const form = document.querySelector('form');
 
 let myLibrary = [];
 
@@ -17,21 +23,13 @@ function Book(title, author, pages, read) {
     };
 }
 
-let Book1 = new Book("Book1", "Bob", 123, "read");
-let Book2 = new Book("Book2", "Cob", 123, "not read");
-let Book3 = new Book("Book3", "Dob", 123, "read");
-
-
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-addBookToLibrary(Book1);
-addBookToLibrary(Book2);
-addBookToLibrary(Book3);
-
 // for each book in myLibrary, creates card and appends book info on card
 function displayBooks() {
+    container.textContent = '';
     myLibrary.forEach(book => {
         let card = document.createElement("div");
         container.appendChild(card); // change container later
@@ -83,3 +81,47 @@ closeBtn.addEventListener('click', () => {
 overlay.addEventListener('click', () => {
     closeModal();
 });
+
+// returns true or false to if all form fields are filled 
+function formAllValid() {
+    if (title.value != '' && author.value != '' && pages.value != ''
+        && (read.checked || notRead.checked)) {
+        return true;
+    } else return false;
+}
+
+// deselects all form fields
+function clearAll() {
+    title.value = ''
+    author.value = ''
+    pages.value = ''
+    read.checked = false
+    notRead.checked = false
+}
+
+// if all inputs are valid
+// collect each piece of data 
+// create new book
+// add to library
+// displayBooks()
+// clear form & close modal
+
+submitBtn.addEventListener('click', () => {
+    if (formAllValid()) {
+        let newBook = new Book(title.value, author.value, pages.value, "read");
+        addBookToLibrary(newBook);
+        displayBooks();
+        clearAll();
+        closeModal();
+    }
+});
+
+// just to have some placeholder books
+let Book1 = new Book("Book1", "Bob", 123, "read");
+let Book2 = new Book("Book2", "Cob", 123, "not read");
+let Book3 = new Book("Book3", "Dob", 123, "read");
+addBookToLibrary(Book1);
+addBookToLibrary(Book2);
+addBookToLibrary(Book3);
+
+displayBooks();
